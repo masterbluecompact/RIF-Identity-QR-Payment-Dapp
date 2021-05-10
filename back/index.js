1625d62a74f9bc4bf2943768ca224adc54344089
t0m <husamirami@gmail.com>
2021-05-11 00:18:48 +0300

    removed unneccessary routes

--- back/index.js
+++ back/index.js
@@ -44,15 +44,6 @@ const expressDIDAuthMiddleware = setupApp({
   signupBusinessLogic,
 })(app);
 
-app.get("/not-protected", function (req, res) {
-  console.log("Not protected triggered!");
-  res.send("This endpoint is not authenticating");
-});
-
-app.get("/protected", expressDIDAuthMiddleware, function (req, res) {
-  console.log(`Protected triggered! (${req.user.did})`);
-  res.send("This endpoint is authenticating");
-});
 
 const port = process.env.SERVICE_PORT;
 
